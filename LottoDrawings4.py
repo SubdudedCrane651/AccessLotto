bb115bea308dcc617a749f0599228f063888125e
SubduedCrane651 <rchrdperreault@gmail.com>
2025-02-22 17:13:58 -0500

    Added more functionality

--- LottoDrawings4.py
+++ LottoDrawings4.py
@@ -1,16 +1,13 @@
 import json
-import numbers
 import random
 import os
 import sys
 import requests
-import threading
+from concurrent.futures import ThreadPoolExecutor
 
-#Best works with pypy3.10 for more performance including threads
-
+# Best works with pypy3.10 for more performance including threads
 
 os.system('cls')
-
 
 def choose(i):
     switcher = {
@@ -76,208 +73,85 @@ url = "https://richard-perreault.com/Documents/" + jsonfile
 response = requests.get(url)
 data = json.loads(response.text)
 
-global count
 count = len(data)
-
-global drawnumbers
 drawnumbers = []
 
 def PrintStatus():
-    sys.stdout.write("|\r");sys.stdout.flush()
-    sys.stdout.write("/\r");sys.stdout.flush()
-    sys.stdout.write("\\\r");sys.stdout.flush()
-    sys.stdout.write("|\r");sys.stdout.flush()
-    sys.stdout.write("/\r");sys.stdout.flush()
+    sys.stdout.write("|\r"); sys.stdout.flush()
+    sys.stdout.write("/\r"); sys.stdout.flush()
+    sys.stdout.write("\\\r"); sys.stdout.flush()
+    sys.stdout.write("|\r"); sys.stdout.flush()
+    sys.stdout.write("/\r"); sys.stdout.flush()
 
-def PickLottoNumbers(samenumber, total,numbers):
+def PickLottoNumbers(samenumber, total, numbers):
+    seen = set(numbers)
+    while samenumber:
+        rnd = random.randint(1, total)
+        if rnd not in seen:
+            seen.add(rnd)
+            numbers.append(rnd)
+            samenumber -= 1
+    numbers.sort()
+    return numbers
 
-    count2 = 0
-    rnd=0
-    samenumber=1
+def PickNum(data, numbers, lotto):
+    hits = 0
+    PickNumbers = True
+    numbers_set = set(numbers)
+    
+    for pan in data:
+        PrintStatus()
 
-    for number in numbers:
-
-        if numbers[count2 - 1] == number and count2 != 0:
-
-         rnd = random.randint(1,total)
-
-         numbers[count2] = rnd
-
-         rnd = random.randint(1,total)
-
-         samenumber += 1
-
+        # Common set operations for various lotto types
+        drawn_numbers = {
+            1: {int(pan.get("P1", 0)), int(pan.get("P2", 0)), int(pan.get("P3", 0)), int(pan.get("P4", 0)), int(pan.get("P5", 0)), int(pan.get("P6", 0))},
+            2: {int(pan.get("P1", 0)), int(pan.get("P2", 0)), int(pan.get("P3", 0)), int(pan.get("P4", 0)), int(pan.get("P5", 0)), int(pan.get("P6", 0)), int(pan.get("P7", 0))},
+            3: {int(pan.get("p1", 0)), int(pan.get("p2", 0)), int(pan.get("p3", 0)), int(pan.get("p4", 0)), int(pan.get("p5", 0)), int(pan.get("gn", 0))},
+            4: {int(pan.get("p1", 0)), int(pan.get("p2", 0)), int(pan.get("p3", 0)), int(pan.get("p4", 0)), int(pan.get("p5", 0)), int(pan.get("p6", 0)), int(pan.get("p7", 0)), int(pan.get("p8", 0)), int(pan.get("p9", 0)), int(pan.get("p10", 0)), int(pan.get("p11", 0)), int(pan.get("p12", 0))}
+        }[lotto]
+        
+        hit = len(numbers_set & drawn_numbers)
+        
+        if (lotto == 1 and hit >= 4) or (lotto == 2 and hit >= 4) or (lotto == 3 and (hit == 5 or hit == 6)) or (lotto == 4 and (hit == 12 or hit >= 8 or hit <= 4)):
+            PickNumbers = True
+            hits += 1
         else:
+            PickNumbers = False
+    return PickNumbers, hits
 
-           samenumber -= 1
+def lotto_drawings(rangenum, drawingnum, same, drawnumbers):
+    while True:
+        numbers = []
+        numbers = PickLottoNumbers(same, drawingnum, numbers)
+        PickNumbers, hits = PickNum(data, numbers, lotto)
+        if PickNumbers:
+            break
+    return numbers
 
-        count2 += 1
+# Use ThreadPoolExecutor for better thread management
+with ThreadPoolExecutor(max_workers=4) as executor:
+    if lotto == 1:
+        print(loto649)
+        lottonumbers = executor.submit(lotto_drawings, 7, 49, 6, drawnumbers).result()
+        os.system('C:\\Users\\rchrd\\AppData\\Local\\Microsoft\\WindowsApps\\python3.9.exe C:\\Users\\rchrd\\Documents\\Python\\text2speech.py "--lang=fr" "Voici les numéros gagnants de Lotto 6/49"')
+        print(f"The winning 6/49 numbers are {lottonumbers} in a total of {count} drawings")
 
-        numbers.sort()
+    if lotto == 2:
+        print(LottoMax)
+        lottonumbers = executor.submit(lotto_drawings, 8, 50, 7, drawnumbers).result()
+        os.system('C:\\Users\\rchrd\\AppData\\Local\\Microsoft\\WindowsApps\\python3.9.exe C:\\Users\\rchrd\\Documents\\Python\\text2speech.py "--lang=fr" "Voici les numéros gagnants de Lotto Max"')
+        print(f"The LottoMax winning numbers are {lottonumbers} in a total of {count} drawings")
 
-    return numbers, samenumber
+    if lotto == 3:
+        print(GrandeVie)
+        lottonumbers = executor.submit(lotto_drawings, 6, 49, 5, drawnumbers).result()
+        os.system('C:\\Users\\rchrd\\AppData\\Local\\Microsoft\\WindowsApps\\python3.9.exe C:\\Users\\rchrd\\Documents\\Python\\text2speech.py "--lang=fr" "Voici les numéros gagnants de la Grande Vie"')
+        print(f"The winning Grande Vie numbers are {lottonumbers} in a total of {count} drawings")
 
-def PickNum(data,numbers,lotto,hits):
-         num=0
-         hit=0
-         PickNumbers=True
-         for pan in data:
-         
-                    PrintStatus()
-         
-                    hit=0
+    if lotto == 4:
+        print(ToutouRien)
+        lottonumbers = executor.submit(lotto_drawings, 13, 24, 12, drawnumbers).result()
+        os.system('C:\\Users\\rchrd\\AppData\\Local\\Microsoft\\WindowsApps\\python3.9.exe C:\\Users\\rchrd\\Documents\\Python\\text2speech.py "--lang=fr" "Voici les numéros gagnants de Tout ou rien"')
+        print(f"The winning Tout ou Rien numbers are {lottonumbers} in a total of {count} drawings")
 
-                    #Lotto 6/49 Drawings
-                    if lotto == 1:
-                        
-                        for num in range(0,6):
-                            if numbers[num] == int(pan["P1"]) or numbers[num] == int(pan["P2"]) \
-                            or numbers[num] == int(pan["P3"]) or numbers[num] == int(pan["P4"]) \
-                            or numbers[num] == int(pan["P5"]) or numbers[num] == int(pan["P6"]) \
-                            or numbers[num] == int(pan["P7"]):
-                             hit+=1
-                        if hit >= 4:
-                            PickNumbers=True
-                            hits+=1
-                        else:
-                            PickNumbers=False
-
-                    #LottoMax Drawings        
-                    if lotto == 2:
-
-                        for num in range(0,7):
-                            if numbers[num] == int(pan["P1"]) or numbers[num] == int(pan["P2"]) \
-                            or numbers[num] == int(pan["P3"]) or numbers[num] == int(pan["P4"]) \
-                            or numbers[num] == int(pan["P5"]) or numbers[num] == int(pan["P6"] \
-                            or numbers[num] == int(pan["P7"])):
-                              hit+=1
-                            if hit >3:
-                               PickNumbers=True
-                               hits+=1
-                            else:
-                               PickNumbers=False 
-
-                    #Grande Vie Drawings
-                    elif lotto == 3:
-
-                        if numbers[0] == int(pan["p1"]) and numbers[1] == int(pan["p2"]) \
-                        and numbers[2] == int(pan["p3"]) and numbers[3] == int(pan["p4"])\
-                        and numbers[4] == int(pan["p5"]):
-                            PickNumbers=True
-                            hits=+1
-                        else:
-                            PickNumbers=False
-
-                        if numbers[0] == int(pan["p1"]) and numbers[1] == int(pan["p2"]) \
-                        and numbers[2] == int(pan["p3"]) and numbers[3] == int(pan["p4"]) \
-                        and numbers[4] == int(pan["p5"]) and numbers[5] == int(pan["gn"]):
-                            PickNumbers=True
-                            hits=+1
-                        else:
-                            PickNumbers=False 
-
-                        for num in range(0,5):
-                            if numbers[num] == int(pan["p1"]) or numbers[num] == int(pan["p2"]) \
-                            or numbers[num] == int(pan["p3"]) or numbers[num] == int(pan["p4"]):       
-                                hit+=1
-                            if hit >= 5:
-                                PickNumbers=True
-                                hits+=1
-                            else:
-                                PickNumbers=False
-            
-                    #Tout ou Rien Drawings
-                    if lotto == 4:
-                        for num in range(0,12):
-                            if numbers[num] == int(pan["p1"]) or numbers[num] == int(pan["p2"]) \
-                            or numbers[num] == int(pan["p3"]) or numbers[num] == int(pan["p4"]) \
-                            or numbers[num] == int(pan["p5"]) or numbers[num] == int(pan["p6"]) \
-                            or numbers[num] == int(pan["p7"]) or numbers[num] == int(pan["p8"]) \
-                            or numbers[num] == int(pan["p9"]) or numbers[num] == int(pan["p10"]) \
-                            or numbers[num] == int(pan["p11"]) or numbers[num] == int(pan["p12"]):          
-                                hit+=1
-                            if hit==12:
-                                PickNumbers=True
-                                hits+=1
-                            else:
-                                PickNumbers=False
-         return PickNumbers,hits
-
-
-class LottoDrawings(threading.Thread):
-
-    def __init__(self, rangenum, drawingnum, same, drawnumbers):
-        threading.Thread.__init__(self)
-        self.rangenum = rangenum
-        self.drawingnum = drawingnum
-        self.same = same
-        self.drawnumbers = drawnumbers
-
-    def run(self):
-        PickNumbers = True
-        hits = 0
-
-        while PickNumbers or hits > 0:
-            numbers = []
-            hits = 0
-
-            for count in range(1, self.rangenum):
-                rnd = random.randint(1, self.drawingnum)
-                samenumber = 0
-                numbers2 = PickLottoNumbers(samenumber, self.drawingnum, numbers)
-                numbers2[0].append(rnd)
-                numbers2[0].sort()
-                samenumber = 0
-
-            while samenumber != self.same:
-                numbers2 = PickLottoNumbers(samenumber, self.drawingnum, numbers)
-                samenumber = numbers2[1]
-            numbers = numbers2[0]
-
-            if self.same == -4:
-                rnd = random.randint(1, 6)
-                numbers.append(rnd)
-
-            self.drawnumbers = numbers
-            PickNumbers, hits = PickNum(data, numbers, lotto, hits)
-
-
-# Lotto 6/49 Drawings
-if lotto == 1:
-    print(loto649)
-    lottonumbers = LottoDrawings(7, 49, -5, drawnumbers)
-    lottonumbers.start()
-    lottonumbers.join()
-    print("The winning 6/49 numbers are " + str(lottonumbers.drawnumbers) + " in a total of " + str(count) + " drawings")
-    os.system('C:\\Users\\rchrd\\AppData\\Local\\Microsoft\\WindowsApps\\python3.9.exe C:\\Users\\rchrd\\Documents\\Python\\text2speech.py "--lang=fr" "Voici les numéros gagnants de Lotto 6/49"')
-    input("<PRESS ENTER>")
-
-# LottoMax Drawings
-if lotto == 2:
-    print(LottoMax)
-    lottonumbers = LottoDrawings(8, 50, -6, drawnumbers)
-    lottonumbers.start()
-    lottonumbers.join()
-    print("The LottoMax winning numbers are " + str(lottonumbers.drawnumbers) + " in a total of " + str(count) + " drawings")
-    os.system('C:\\Users\\rchrd\\AppData\\Local\\Microsoft\\WindowsApps\\python3.9.exe C:\\Users\\rchrd\\Documents\\Python\\text2speech.py "--lang=fr" "Voici les numéros gagnants de Lotto Max"')
-    input("<PRESS ENTER>")
-
-# Grande Vie Drawings
-if lotto == 3:
-    print(GrandeVie)
-    lottonumbers = LottoDrawings(6, 49, -4, drawnumbers)
-    lottonumbers.start()
-    lottonumbers.join()
-    print("The winning Grande Vie numbers are " + str(lottonumbers.drawnumbers) + " in a total of " + str(count) + " drawings")
-    os.system('C:\\Users\\rchrd\\AppData\\Local\\Microsoft\\WindowsApps\\python3.9.exe C:\\Users\\rchrd\\Documents\\Python\\text2speech.py "--lang=fr" "Voici les numéros gagnants de la Grande Vie"')
-    input("<PRESS ENTER>")
-
-# Tout ou Rien Drawings
-if lotto == 4:
-    print(ToutouRien)
-    lottonumbers = LottoDrawings(13, 24, -11, drawnumbers)
-    lottonumbers.start()
-    lottonumbers.join()
-    print("The winning Tout ou Rien numbers are " + str(lottonumbers.drawnumbers) + " in a total of " + str(count) + " drawings")
-    os.system('C:\\Users\\rchrd\\AppData\\Local\\Microsoft\\WindowsApps\\python3.9.exe C:\\Users\\rchrd\\Documents\\Python\\text2speech.py "--lang=fr" "Voici les numéros gagnants de Tout ou rien"')
-    input("<PRESS ENTER>")
+input("<PRESS ENTER>")
